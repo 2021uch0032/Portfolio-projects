4234d0a4c823a4bdb13dd3db103209f48a5cd482
Shrey Srivastava <160382208+2021uch0032@users.noreply.github.com>
2024-10-04 07:11:18 +0530

    Update Tableau Portfolio Project SQL Queries.sql

--- Tableau Portfolio Project SQL Queries.sql
+++ Tableau Portfolio Project SQL Queries.sql
@@ -1,16 +1,11 @@
 /*
-
 Queries used for Tableau Project
-
 */
-
-
-
 -- 1. 
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
---Where location like '%states%'
+--Where location like '%India%'
 where continent is not null 
 --Group By date
 order by 1,2
